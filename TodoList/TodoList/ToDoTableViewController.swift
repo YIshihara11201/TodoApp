af4cf197bee73a481a9a76ec31dd5f1ec00ca238
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2022-05-17 08:46:05 -0700

    initial commit

---
+++ TodoList/TodoList/ToDoTableViewController.swift
@@ -0,0 +1,81 @@
+//
+//  TodoTableViewController.swift
+//  TodoList
+//
+//  Created by Yusuke Ishihara on 2022-05-16.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos = [ToDo]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        if let saveToDos = ToDo.loadToDos() {
+            toDos = saveToDos
+        } else {
+            toDos = ToDo.loadSampleToDos()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath) as! ToDoCell
+        
+        let toDo = toDos[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = toDo.title
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func unwindToToDoList(segue: UIStoryboardSegue) {
+        guard segue.identifier == "saveUnwind" else { return }
+        let sourceViewController = segue.source as! TodDoDetailTableViewController
+        
+        if let toDo = sourceViewController.toDo {
+            if let indexOfExistingToDo = toDos.firstIndex(of: toDo) {
+                toDos[indexOfExistingToDo] = toDo
+                tableView.reloadRows(at: [IndexPath(row: indexOfExistingToDo,
+                                                    section: 0)], with: .automatic)
+            } else {
+                let newIndexPath = IndexPath(row: toDos.count, section: 0)
+                toDos.append(toDo)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        }
+    }
+    
+    @IBSegueAction func editToDo(_ coder: NSCoder, sender: Any?) -> TodDoDetailTableViewController? {
+        let detailController = TodDoDetailTableViewController(coder: coder)
+        
+        guard let cell = sender as? UITableViewCell,
+              let indexPath = tableView.indexPath(for: cell) else {
+            return detailController
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        detailController?.toDo = toDos[indexPath.row]
+        
+        return detailController
+    }
+}
